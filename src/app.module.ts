71538d0f2a02815c8d38914071d322eb6eeac7db
Mahmoud_nfz <mahmoudhnefzi@gmail.com>
2023-04-19 18:14:30 +0100

    [Add] Database connection, ConfigModule and Prettier formatting

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { ConfigModule } from '@nestjs/config'
+import { TypeOrmModule } from '@nestjs/typeorm'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [
+		ConfigModule.forRoot({
+			envFilePath: '.env',
+			isGlobal: true,
+		}),
+		TypeOrmModule.forRoot({
+			type: 'mysql',
+			host: process.env.DB_HOST,
+			port: parseInt(process.env.DB_PORT),
+			username: process.env.DB_USERNAME,
+			password: process.env.DB_PASSWORD,
+			database: process.env.DB_DATABASE,
+			entities: [__dirname + '/**/*.entity{.ts,.js}'],
+			synchronize: true,
+		}),
+	],
+	controllers: [AppController],
+	providers: [AppService],
 })
 export class AppModule {}
